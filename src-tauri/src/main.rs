9391f3ebafeb3ec463f21730eba26ecbc2585bb2
Felipe Rieck <frieck@atlassian.com>
2023-03-29 14:23:48 -0300

    Adding transparency

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -2,6 +2,8 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
 use std::env;
+use tauri::Manager;
+use window_vibrancy::{apply_blur, apply_vibrancy, NSVisualEffectMaterial};
 
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
 #[tauri::command]
@@ -16,6 +18,19 @@ fn app_started() -> String {
 fn main() {
     tauri::Builder::default()
         .invoke_handler(tauri::generate_handler![app_started])
+        .setup(|app| {
+            let window = app.get_window("main").unwrap();
+      
+            #[cfg(target_os = "macos")]
+            apply_vibrancy(&window, NSVisualEffectMaterial::HudWindow, None, None)
+              .expect("Unsupported platform! 'apply_vibrancy' is only supported on macOS");
+      
+            #[cfg(target_os = "windows")]
+            apply_blur(&window, Some((18, 18, 18, 125)))
+              .expect("Unsupported platform! 'apply_blur' is only supported on Windows");
+      
+            Ok(())
+          })
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
